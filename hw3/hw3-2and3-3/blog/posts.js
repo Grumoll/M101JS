50f3dcdd6c194d821695c3ec0e972ca5a2cfb4f8
Grumoll <Grumoll@users.noreply.github.com>
2015-04-03 17:05:41 +0200

    Update posts.js

--- hw3/hw3-2and3-3/blog/posts.js
+++ hw3/hw3-2and3-3/blog/posts.js
@@ -30,7 +30,16 @@ function PostsDAO(db) {
 
         // now insert the post
         // hw3.2 TODO
-        posts.insert(post, callback);
+        posts.insert(post, function (err, result) {
+            "use strict";
+
+            if (!err) {
+                console.log("Inserted new post");
+                return callback(null, permalink);
+            }
+
+            return callback(err, null);
+        });
         //callback(Error("insertEntry NYI"), null);
     }
 
